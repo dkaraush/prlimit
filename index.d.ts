bb124c248b1da8fa0fd37c9fb4a82bcd41a5f836
dkaraush <dkaraush@gmail.com>
2020-06-08 22:49:10 +0300

    fixed types x2; changed name to

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,10 @@
-declare module "node-prlimit" {
-	export interface Limit {
+declare module "prlimit" {
+	interface Limit {
 		hard: number | null;
 		soft: number | null;
 	}
 
-	export type ResourceLimit = "cpu" | "data" | "fsize" | "locks" |
+	type ResourceLimit = "cpu" | "data" | "fsize" | "locks" |
 		"memlock" | "msgqueue" | "nice" | "nofile" |
 		"nproc" | "rss" | "rtprio" | "rttime" | "sigpending";
 
